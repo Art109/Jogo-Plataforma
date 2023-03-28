1ffac0990795bd12829d4cd5bdb1b47f6e4569df
Arthur Aguiar <arthur.espinaguiar@gmail.com>
2023-03-28 10:53:11 -0300

    Inimigo Basico Finalizado

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -14,10 +14,69 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] Animator animator;
 
+
+    //Movimento + IA simples
+    Rigidbody2D rb;
+    [SerializeField] float speed = 2f;
+    [SerializeField] bool isRight = false;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+   
+
     void Start()
     {
+        rb= GetComponent<Rigidbody2D>();
         currentHealt = maxHealth;
     }
+
+    void Update()
+    {
+        Movimento();
+    }
+
+
+
+    void Movimento()
+    {
+        animator.SetBool("Run", true);
+        if(isRight)
+        {
+            rb.velocity = new Vector2(speed,rb.velocity.y);
+        }
+        if (!isRight)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+
+
+        if (!isGrounded())
+        {
+            Flip();
+        }
+        
+        
+        
+    }
+
+    void Flip()
+    {
+        if (isRight && rb.velocity.x < 0 || !isRight && rb.velocity.x > 0)
+        {
+            Vector2 localScale = transform.localScale;
+            isRight = !isRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+
+
 
     public void TakeDamage(int damage)
     {
@@ -55,6 +114,9 @@ public class Enemy : MonoBehaviour
             Debug.Log("Acertei o player");
         }
     }
+
+
+
     
 
 }
